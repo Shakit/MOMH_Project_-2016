9d432b433b276129da6f18e2fe9255692067d492
Potalas <potalas@free.fr>
2016-10-22 01:05:28 +0200

    Add files via upload
    execution command:
    Rscript graph.R test.txt

---
+++ graph.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+file.names<-args[1]
+
+data<-read.table(file=file.names[1], header=TRUE, sep=":")
+x<-data[,1]
+y<-data[,2]
+xmax<-max(x)*2
+ymax<-max(y)*2
+png(file="results.png", width=1600, heigh=800)
+par(mfrow=c(1,1))
+plot(x,y, main="Solutions", type="p",xlim=c(0,xmax), ylim=c(0,ymax), lwd = 5
+, col="red")
+
